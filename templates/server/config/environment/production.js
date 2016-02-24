7ab0090af9ea6bee541035c468d3bf39b3b87e1e
Preston Van Loon <preston@machinepowered.com>
2016-02-24 15:52:10 -0600

    Removed HTML rendering and docs

--- templates/server/config/environment/production.js
+++ templates/server/config/environment/production.js
@@ -4,7 +4,7 @@
   module.exports = {
     port: process.env.PORT || fatal('PORT not defined')<% if(mongoose) { %>,
     mongo: {
-      uri: process.env.MONGO_URI || fatal('MONGO_URI not defined'),
+      uri: process.env.MONGO_URI || process.env.MONGOLAB_URI || fatal('MONGO_URI not defined'),
       autoIndex: false
     }<% } %>
   };
--- templates/server/config/express.js
+++ templates/server/config/express.js
@@ -13,19 +13,14 @@
   module.exports = function(app) {
     app.disable('x-powered-by');
 
-    let env = app.get('env');
-
-    app.engine('html', require('ejs').renderFile);
-    app.set('view engine', 'html');
     app.use(compression());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use(methodOverride());
     app.use(morgan('dev'));
 
-    app.use('/docs', express.static('docs'));
-    app.set('appPath', path.join(config.root, 'docs'));
-
+    let env = app.get('env');
+    
     /* istanbul ignore next */
     if(env === 'development' || env === 'test') {
       app.use(require('connect-livereload')());
